ec327d710b6f802397501c8a7f23047f93445d3e
Vladimir Vivien <vladimir.vivien@gmail.com>
2023-07-01 17:32:55 -0400

    Moving away from unstructured underlying type (once again)

--- objmeta/objmeta.go
+++ objmeta/objmeta.go
@@ -2,8 +2,11 @@
 package objmeta
 
 import (
+	"strings"
+
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
 var (
@@ -14,39 +17,68 @@ var (
 )
 
 // Builder provides a way to build values of type coreV1.ObjectMeta
-type Builder map[string]interface{}
-
-// Name sets the name of the object
-func Name(name string) Builder {
-	return Builder{"name": name}
+type Builder struct {
+	obj metaV1.ObjectMeta
 }
 
-func (b Builder) T() (metaV1.ObjectMeta, error) {
+// From creates a new builder using the provided metaV1.ObjectMeta as its base
+func From(obj metaV1.ObjectMeta) Builder {
+	return Builder{obj: obj}
+}
+
+// FromUnstructured attempts to convert an unstructured value into metaV1.ObjectMeta
+// and uses it as the basis for a Builder
+func FromUnstructured(unstruct map[string]any) (Builder, error) {
 	var obj metaV1.ObjectMeta
-	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(b, &obj); err != nil {
-		return ObjectMetaNone, err
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(unstruct, &obj); err != nil {
+		return Builder{}, err
 	}
-	return obj, nil
+	return Builder{obj: obj}, nil
 }
 
-func (b Builder) U() map[string]interface{} {
-	return b
+// FromString attempts to convert the provided YAML or JSON string fragment
+// into a valid metaV1.ObjectMeta and uses it as the basis for the builder
+func FromString(str string) (Builder, error) {
+	var obj metaV1.ObjectMeta
+	if err := yaml.NewYAMLOrJSONDecoder(strings.NewReader(str), 1024).Decode(&obj); err != nil {
+		return Builder{}, err
+	}
+	return Builder{obj: obj}, nil
+}
+
+// Name starts a new builer by setting the Objectmeta name of the object
+func Name(name string) Builder {
+	return Builder{obj: metaV1.ObjectMeta{Name: name}}
+}
+
+// U converts the value of the builder to an unstructured map[string]any value
+func (b Builder) U() (map[string]any, error) {
+	unstruct, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&b.obj)
+	if err != nil {
+		return nil, err
+	}
+	return unstruct, nil
+}
+
+// T returns the typed value of the builder of metaV1.ObjectMeta
+func (b Builder) T() metaV1.ObjectMeta {
+	return b.obj
 }
 
 // Namespace value setter
 func (b Builder) Namespace(ns string) Builder {
-	b["namespace"] = ns
+	b.obj.Namespace = ns
 	return b
 }
 
 // Labels value setter
 func (b Builder) Labels(labels map[string]string) Builder {
-	b["labels"] = labels
+	b.obj.Labels = labels
 	return b
 }
 
 // Annotations setter
 func (b Builder) Annotations(annotations map[string]string) Builder {
-	b["annotations"] = annotations
+	b.obj.Annotations = annotations
 	return b
 }
--- objmeta/objmeta_test.go
+++ objmeta/objmeta_test.go
@@ -7,44 +7,7 @@ import (
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestObjectMetaUnstructuredValues(t *testing.T) {
-	tests := map[string]struct {
-		builder  Builder
-		expected map[string]interface{}
-	}{
-		"empty object": {
-			builder:  Builder{},
-			expected: map[string]interface{}{},
-		},
-		"name only": {
-			builder:  Name("simple-name"),
-			expected: map[string]interface{}{"name": "simple-name"},
-		},
-		"name and namespace": {
-			builder:  Name("simple-name").Namespace("my-namespace"),
-			expected: map[string]interface{}{"name": "simple-name", "namespace": "my-namespace"},
-		},
-		"name and namespace and labels": {
-			builder:  Name("simple-name").Namespace("my-namespace").Labels(map[string]string{"tier": "web"}),
-			expected: map[string]interface{}{"name": "simple-name", "namespace": "my-namespace", "labels": map[string]string{"tier": "web"}},
-		},
-		"all fields": {
-			builder:  Name("simple-name").Namespace("my-namespace").Labels(map[string]string{"tier": "web"}).Annotations(map[string]string{"status": "ready"}),
-			expected: map[string]interface{}{"name": "simple-name", "namespace": "my-namespace", "labels": map[string]string{"tier": "web"}, "annotations": map[string]string{"status": "ready"}},
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			objMeta := test.builder.U()
-			if !reflect.DeepEqual(objMeta, test.expected) {
-				t.Errorf("object not equal \n\n Constructor: %#v \n\n Expected: %#v", objMeta, test.expected)
-			}
-		})
-	}
-}
-
-func TestObjectMetaTypedValues(t *testing.T) {
+func TestObjectMeta(t *testing.T) {
 	tests := map[string]struct {
 		builder  Builder
 		expected metaV1.ObjectMeta
@@ -69,14 +32,26 @@ func TestObjectMetaTypedValues(t *testing.T) {
 			builder:  Name("simple-name").Namespace("my-namespace").Labels(map[string]string{"tier": "web"}).Annotations(map[string]string{"status": "ready"}),
 			expected: metaV1.ObjectMeta{Name: "simple-name", Namespace: "my-namespace", Labels: map[string]string{"tier": "web"}, Annotations: map[string]string{"status": "ready"}},
 		},
+		"from unstructured": {
+			builder: func() Builder {
+				b, _ := FromUnstructured(map[string]any{"name": "simple-name", "namespace": "my-namespace"})
+				return b
+			}(),
+			expected: metaV1.ObjectMeta{Name: "simple-name", Namespace: "my-namespace"},
+		},
+		"from string": {
+			builder: func() Builder {
+				b, _ := FromString(`{"name":"simple-name", "namespace":"my-namespace"}`)
+				return b
+			}(),
+			expected: metaV1.ObjectMeta{Name: "simple-name", Namespace: "my-namespace"},
+		},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			objMeta, err := test.builder.T()
-			if err != nil {
-				t.Fatalf("typed conversion failed: %s", err)
-			}
+			objMeta := test.builder.T()
+
 			if !reflect.DeepEqual(objMeta, test.expected) {
 				t.Errorf("object not equal \n\n Constructor: %#v \n\n Expected: %#v", objMeta, test.expected)
 			}
